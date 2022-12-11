41816dbdd10c0fffe5bf77a3a5e10422901797f3
MayronME <mayron795@gmail.com>
2022-12-11 10:32:11 -0300

    reset

--- carga.py
+++
@@ -1,9 +0,0 @@
-import http
-from locust import FastHttpUser, task
-
-class WebsiteUser(FastHttpUser):
-    http = "http://127.0.0.1:8089"
-
-    @task
-    def index(self):
-        self.client.get("/")
